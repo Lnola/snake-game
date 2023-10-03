cde61d1cafc2342375b8bac9c4c54bc2263b9ef3
Lnola <nola.luka@gmail.com>
2023-10-03 23:30:33 +0200

    :sparkles: Introduce playable height and width instead of constant subtraction

--- src/constants/game-rules.ts
+++ src/constants/game-rules.ts
@@ -3,3 +3,5 @@ export const STEP_INCREMENT = 10;
 export const GAME_BOX_WIDTH = 500;
 export const GAME_BOX_HEIGHT = 500;
 export const POINT_SIZE = 20;
+export const PLAYABLE_WIDTH = GAME_BOX_WIDTH - POINT_SIZE;
+export const PLAYABLE_HEIGHT = GAME_BOX_HEIGHT - POINT_SIZE;

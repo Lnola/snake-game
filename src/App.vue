2070415d082d6172f22f455447c520e1651c3e63
Lnola <nola.luka@gmail.com>
2023-10-03 22:43:54 +0200

    :lipstick: Move the elements to the center

--- src/App.vue
+++ src/App.vue
@@ -40,3 +40,15 @@ const StyledSnakeChunk = styled.span(
   `,
 );
 </script>
+
+<style>
+body {
+  margin: 0;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>

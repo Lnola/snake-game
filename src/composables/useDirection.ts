07e083233fa8c2c9457174d145f57d23b015aacf
Lnola <nola.luka@gmail.com>
2023-10-03 19:25:31 +0200

    :sparkles: Add timeout to direction change

--- src/composables/useDirection.ts
+++ src/composables/useDirection.ts
@@ -1,24 +1,41 @@
 import { ref } from 'vue';
 import { onKeyStroke } from '@vueuse/core';
 import { Directions, type Direction } from '@/types/direction';
+import { MOVEMENT_SPEED } from '@/constants/game-rules';
 
 const useDirection = () => {
   const direction = ref<Direction>(Directions.RIGHT);
+  const isTimedOut = ref<boolean>(false);
+
+  const handleTimeout = () => {
+    isTimedOut.value = true;
+    setTimeout(() => {
+      isTimedOut.value = false;
+    }, MOVEMENT_SPEED);
+  };
 
   onKeyStroke(['w', 'W', 'ArrowUp'], () => {
-    if (direction.value != Directions.BOTTOM) direction.value = Directions.TOP;
+    if (direction.value === Directions.BOTTOM || isTimedOut.value) return;
+    direction.value = Directions.TOP;
+    handleTimeout();
   });
 
   onKeyStroke(['s', 'S', 'ArrowDown'], () => {
-    if (direction.value != Directions.TOP) direction.value = Directions.BOTTOM;
+    if (direction.value === Directions.TOP || isTimedOut.value) return;
+    direction.value = Directions.BOTTOM;
+    handleTimeout();
   });
 
   onKeyStroke(['a', 'A', 'ArrowLeft'], () => {
-    if (direction.value != Directions.RIGHT) direction.value = Directions.LEFT;
+    if (direction.value === Directions.RIGHT || isTimedOut.value) return;
+    direction.value = Directions.LEFT;
+    handleTimeout();
   });
 
   onKeyStroke(['d', 'D', 'ArrowRight'], () => {
-    if (direction.value != Directions.LEFT) direction.value = Directions.RIGHT;
+    if (direction.value === Directions.LEFT || isTimedOut.value) return;
+    direction.value = Directions.RIGHT;
+    handleTimeout();
   });
 
   return { direction };

68a1c5c7d22ae7f41b12e88ae2de18a819c37bda
Lnola <nola.luka@gmail.com>
2023-10-03 18:43:52 +0200

    :technologist: Add global imports

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     port: 3000,
   },

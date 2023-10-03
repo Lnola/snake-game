e15c98141c45112565c45ca3f76196c51e0fc658
Lnola <nola.luka@gmail.com>
2023-10-03 19:16:39 +0200

    :label: Create a direction type

--- src/constants/direction.ts
+++ src/constants/direction.ts
@@ -1,6 +1,10 @@
-export const Direction = {
-  TOP: 'top',
-  BOTTOM: 'bottom',
-  LEFT: 'left',
-  RIGHT: 'right',
-};
+import { ValuesType } from 'utility-types';
+
+export const Directions = {
+  TOP: 'TOP',
+  BOTTOM: 'BOTTOM',
+  LEFT: 'LEFT',
+  RIGHT: 'RIGHT',
+} as const;
+
+export type Direction = ValuesType<typeof Directions>;

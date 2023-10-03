db7c040a9e2a317e3b324bd935cbb1fda6ebe70c
Lnola <nola.luka@gmail.com>
2023-10-03 22:23:35 +0200

    :art: Format the styles

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,6 @@ import { MOVEMENT_SPEED } from '@/constants/game-rules';
 const level = ref(1);
 
 const { direction: lastInputDirection } = useDirection();
-
 const { snakeChunk: head, topInPx, leftInPx, move } = useMovable();
 const gameLoop = ref<Nullable<ReturnType<typeof setInterval>>>(null);
 
@@ -28,10 +27,7 @@ onBeforeUnmount(() => {
 </script>
 
 <script lang="ts">
-type StyledSnakeChunk = BaseProps & {
-  top: string;
-  left: string;
-};
+type StyledSnakeChunk = BaseProps & { top: string; left: string };
 const StyledSnakeChunk = styled.span(
   ({ top, left }: StyledSnakeChunk) => css`
     display: inline-block;
